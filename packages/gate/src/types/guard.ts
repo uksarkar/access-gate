4d0e4aa3724b81b81b9c1451eebbf74162a9effb
Utpal Sarkar <utpalsarkar010@gmail.com>
2024-12-22 21:30:48 +0600

    type updated

--- packages/gate/src/types/guard.ts
+++ packages/gate/src/types/guard.ts
@@ -1,22 +1,21 @@
 export type Guard<T = unknown, D = unknown> = (
-    representative: T,
-    inject: (name: string, dependency: D) => void
-  ) => boolean | undefined;
-  
-  export type LazyGuard<T = unknown, E = unknown, D = unknown> = (
-    representative: T,
-    entity: E,
-    inject: (name: string, dependency: D) => void
-  ) => boolean | undefined;
-  
-  export type AsyncGuard<T = unknown, D = unknown> = (
-    representative: T,
-    inject: (name: string, dependency: D) => void
-  ) => Promise<boolean | undefined>;
-  
-  export type AsyncLazyGuard<T = unknown, E = unknown, D = unknown> = (
-    representative: T,
-    entity: E,
-    inject: (name: string, dependency: D) => void
-  ) => Promise<boolean | undefined>;
-  
+  representative: T,
+  provide: (name: string, dependency: D) => void
+) => boolean | undefined;
+
+export type LazyGuard<T = unknown, E = unknown, D = unknown> = (
+  representative: T,
+  entity: E,
+  provide: (name: string, dependency: D) => void
+) => boolean | undefined;
+
+export type AsyncGuard<T = unknown, D = unknown> = (
+  representative: T,
+  provide: (name: string, dependency: D) => void
+) => Promise<boolean | undefined>;
+
+export type AsyncLazyGuard<T = unknown, E = unknown, D = unknown> = (
+  representative: T,
+  entity: E,
+  provide: (name: string, dependency: D) => void
+) => Promise<boolean | undefined>;
